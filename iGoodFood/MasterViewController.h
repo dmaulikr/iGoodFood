2a07a22cf232e4f7ae89a55e73c472421e31b187
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-13 14:16:19 +0300

    Initial Commit

---
+++ iGoodFood/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  iGoodFood
+//
+//  Created by Ivelin Ivanov on 9/13/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
